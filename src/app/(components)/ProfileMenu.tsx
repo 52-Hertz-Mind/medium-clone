e145cad2958ec378ad93615833ee2eb61b0f1961
Mohammad Aghaei <65438728+MrAghaei@users.noreply.github.com>
2025-01-08 17:10:25 +0330

    [#1-IMPLEMENT] header
    [#1-UPDATE] profile menu sizing

--- src/app/(components)/ProfileMenu.tsx
+++ src/app/(components)/ProfileMenu.tsx
@@ -20,71 +20,74 @@ export default function ProfileMenu() {
           <span className="sr-only">Open menu</span>
         </button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-[280px] text-gray-600" align="end">
-        <div className="grid gap-0.5">
-          <DropdownMenuItem className="flex gap-3 py-3 cursor-pointer ">
-            <User className="h-[18px] w-[18px] stroke-[1.5px] " />
+      <DropdownMenuContent
+        className="w-[280px] text-gray-600 px-5 py-4"
+        align="end"
+      >
+        <div className="flex flex-col">
+          <DropdownMenuItem className="flex gap-3 py-3 cursor-pointer !bg-transparent">
+            <User className="stroke-[1px] !w-6 !h-6" />
             <span>Profile</span>
           </DropdownMenuItem>
-          <DropdownMenuItem className="flex gap-3 py-3 cursor-pointer">
-            <Bookmark className="h-[18px] w-[18px] stroke-[1.5px]" />
+          <DropdownMenuItem className="flex gap-3 py-3 cursor-pointer !bg-transparent">
+            <Bookmark className="stroke-[1px] !w-6 !h-6" />
             <span>Library</span>
           </DropdownMenuItem>
-          <DropdownMenuItem className="flex gap-3 py-3 cursor-pointer">
-            <FileText className="h-[18px] w-[18px] stroke-[1.5px]" />
+          <DropdownMenuItem className="flex gap-3 py-3 cursor-pointer !bg-transparent">
+            <FileText className="stroke-[1px] !w-6 !h-6" />
             <span>Stories</span>
           </DropdownMenuItem>
-          <DropdownMenuItem className="flex gap-3 py-3 cursor-pointer">
-            <BarChart2 className="h-[18px] w-[18px] stroke-[1.5px]" />
+          <DropdownMenuItem className="flex gap-3 py-3 cursor-pointer !bg-transparent">
+            <BarChart2 className="stroke-[1px] !w-6 !h-6" />
             <span>Stats</span>
           </DropdownMenuItem>
         </div>
 
         <DropdownMenuSeparator className="my-1" />
 
-        <div className="grid gap-0.5">
-          <DropdownMenuItem className="py-3 cursor-pointer">
+        <div className="flex flex-col">
+          <DropdownMenuItem className="py-3 cursor-pointer !bg-transparent">
             Settings
           </DropdownMenuItem>
-          <DropdownMenuItem className="py-3 cursor-pointer">
+          <DropdownMenuItem className="py-3 cursor-pointer !bg-transparent">
             Refine recommendations
           </DropdownMenuItem>
-          <DropdownMenuItem className="py-3 cursor-pointer">
+          <DropdownMenuItem className="py-3 cursor-pointer !bg-transparent">
             Manage publications
           </DropdownMenuItem>
-          <DropdownMenuItem className="py-3 cursor-pointer">
+          <DropdownMenuItem className="py-3 cursor-pointer !bg-transparent">
             Help
           </DropdownMenuItem>
         </div>
 
         <DropdownMenuSeparator className="my-1" />
 
-        <div className="grid gap-0.5">
-          <DropdownMenuItem className="flex items-center justify-between py-3 cursor-pointer">
+        <div className="flex flex-col">
+          <DropdownMenuItem className="flex items-center justify-between py-3 cursor-pointer !bg-transparent">
             <span>Become a Medium member</span>
             <Plus className="h-[18px] w-[18px] stroke-[1.5px] text-amber-500" />
           </DropdownMenuItem>
-          <DropdownMenuItem className="py-3 cursor-pointer">
+          <DropdownMenuItem className="py-3 cursor-pointer !bg-transparent">
             Create a Mastodon account
           </DropdownMenuItem>
-          <DropdownMenuItem className="py-3 cursor-pointer">
+          <DropdownMenuItem className="py-3 cursor-pointer !bg-transparent">
             Apply for author verification
           </DropdownMenuItem>
-          <DropdownMenuItem className="py-3 cursor-pointer">
+          <DropdownMenuItem className="py-3 cursor-pointer !bg-transparent">
             Apply to the Partner Program
           </DropdownMenuItem>
-          <DropdownMenuItem className="py-3 cursor-pointer">
+          <DropdownMenuItem className="py-3 cursor-pointer !bg-transparent">
             Gift a membership
           </DropdownMenuItem>
         </div>
 
         <DropdownMenuSeparator className="my-1" />
 
-        <div className="grid gap-0.5">
-          <DropdownMenuItem className="py-3 cursor-pointer">
+        <div className="flex flex-col">
+          <DropdownMenuItem className="py-3 cursor-pointer !bg-transparent">
             Sign out
           </DropdownMenuItem>
-          <DropdownMenuLabel className="py-3 font-normal text-sm text-neutral-500">
+          <DropdownMenuLabel className="py-3 font-normal text-sm text-neutral-500 !bg-transparent">
             mm••••••••••@gmail.com
           </DropdownMenuLabel>
         </div>
