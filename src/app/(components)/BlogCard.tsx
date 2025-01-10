cb66438e4a739f6724018e7b3dfddc5687381b1d
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2025-01-10 13:05:46 +0330

    [#3-UPDATE] blog card

--- src/app/(components)/BlogCard.tsx
+++ src/app/(components)/BlogCard.tsx
@@ -1,5 +1,53 @@
-function BlogCard() {
-  return <div>content</div>;
+import { Hand, MessageCircle } from "lucide-react";
+
+export interface BlogCardDataModel {
+  userName: string;
+  userImageUrl: string;
+  blogTitle: string;
+  blogDescription: string;
+  date: string;
+  clapsCount: number;
+  commentsCount: number;
+  blogImageUrl: string;
+}
+
+interface BlogCardProps {
+  data: BlogCardDataModel;
+}
+
+function BlogCard({ data }: BlogCardProps) {
+  return (
+    <div className="flex gap-8 items-center border-b border-b-gray-100 p-5 mx-10">
+      <div className="flex flex-col gap-3">
+        {/*user data*/}
+        <div className="flex items-center gap-2 ">
+          <img
+            src={data.userImageUrl}
+            className="size-5 rounded-full"
+            alt="profile pic"
+          />
+          <button className="text-sm font-light hover:underline">
+            {data.userName}
+          </button>
+        </div>
+        <h2 className="text-2xl font-bold">{data.blogTitle}</h2>
+        <p className="text-sm text-gray-600">{data.blogDescription}</p>
+        {/*blog stats*/}
+        <div className="flex items-center gap-5 mt-3">
+          <span className="text-xs font-light text-gray-500">{data.date}</span>
+          <div className="flex gap-1 items-center">
+            <Hand className="size-4 text-black" strokeWidth={1} />
+            <span className="text-sm text-gray-500">{data.clapsCount}</span>
+          </div>
+          <div className="flex gap-1 items-center">
+            <MessageCircle className="size-4 text-black" strokeWidth={1} />
+            <span className="text-sm text-gray-500">{data.clapsCount}</span>
+          </div>
+        </div>
+      </div>
+      <img src={data.blogImageUrl} className="w-40" alt="blog image" />
+    </div>
+  );
 }
 
 export default BlogCard;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,26 @@
 import TabNavigation from "@/app/(components)/TabNavigation";
-import BlogCard from "@/app/(components)/BlogCard";
+import BlogCard, { BlogCardDataModel } from "@/app/(components)/BlogCard";
 
 export default function Home() {
+  const data: BlogCardDataModel = {
+    userName: "mamad",
+    userImageUrl: "/profilepic.jpg",
+    blogTitle: "How I Tricked My Brain to Be Addicted to Coding",
+    blogDescription:
+      "Have you ever wondered if coding could be as addictive and fun as playing video games",
+    date: "Sep 22, 2021",
+    clapsCount: 21,
+    commentsCount: 31,
+    blogImageUrl: "/blogImage.png",
+  };
   return (
-    <div className=" justify-center flex gap-10 px-96">
-      <div className="w-3/4 border-r h-screen pt-10">
+    <div className="justify-center flex gap-10 px-96">
+      <div className="flex flex-col items-start w-3/4 border-r h-screen pt-10 gap-5">
         <TabNavigation
           linkNames={["For you", "Programming", "Web"]}
           isPlusNeeded={true}
         />
-        <BlogCard />
+        <BlogCard data={data} />
       </div>
       <div className="w-1/3 pt-10">sidebar</div>
     </div>
