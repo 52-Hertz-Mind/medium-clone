d6b99daaf83c930396559e9e8ae4d2ba3a0bd155
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2025-01-10 12:24:02 +0330

    [#3-UPDATE] main page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import TabNavigation from "@/app/(components)/TabNavigation";
+import BlogCard from "@/app/(components)/BlogCard";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center py-3">
-      <TabNavigation
-        linkNames={["For you", "Programming", "Web"]}
-        isPlusNeeded={true}
-      />
+    <div className=" justify-center flex gap-10 px-96">
+      <div className="w-3/4 border-r h-screen pt-10">
+        <TabNavigation
+          linkNames={["For you", "Programming", "Web"]}
+          isPlusNeeded={true}
+        />
+        <BlogCard />
+      </div>
+      <div className="w-1/3 pt-10">sidebar</div>
     </div>
   );
 }
