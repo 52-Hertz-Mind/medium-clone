d809539ba54abf4591164e251a35fbb3676c6f8c
Mohammad Aghaei <65438728+MrAghaei@users.noreply.github.com>
2025-01-10 11:47:00 +0330

    [#2-ADD] tabs navigation component (#5)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import TabNavigation from "@/app/(components)/TabNavigation";
+
 export default function Home() {
-  return <div>main</div>;
+  return (
+    <div className="flex flex-col items-center justify-center py-3">
+      <TabNavigation
+        linkNames={["For you", "Programming", "Web"]}
+        isPlusNeeded={true}
+      />
+    </div>
+  );
 }
