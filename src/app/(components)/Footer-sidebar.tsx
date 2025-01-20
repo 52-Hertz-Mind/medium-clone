214129afe86c34b3f22b16f6ed544405ffe8478a
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2025-01-20 16:34:06 +0330

    [#6-REFACTOR] footer component

--- src/app/(components)/Footer-sidebar.tsx
+++ src/app/(components)/Footer-sidebar.tsx
@@ -1,38 +1,29 @@
 function FooterSidebar() {
+  const footerButtonsNames = [
+    "Help",
+    "Status",
+    "About",
+    "Careers",
+    "Press",
+    "Blog",
+    "Privacy",
+    "Terms",
+  ];
   return (
     <div className="flex flex-wrap w-72 items-start text-[11px] ">
       <div className="flex ">
-        <div className="mr-1">
-          <a className="cursor-pointer text-gray-700 ">Help</a>
-        </div>
-        <div className="mr-1">
-          <a className="cursor-pointer text-gray-700 ">Status</a>
-        </div>
-        <div className="mr-1">
-          <a className="cursor-pointer text-gray-700 ">About</a>
-        </div>
-        <div className="mr-1">
-          <a className="cursor-pointer text-gray-700 ">Careers</a>
-        </div>
-        <div className="mr-1">
-          <a className="cursor-pointer text-gray-700 ">Press</a>
-        </div>
-        <div className="mr-1">
-          <a className="cursor-pointer text-gray-700 ">Blog</a>
-        </div>
-        <div className="mr-1">
-          <a className="cursor-pointer text-gray-700 ">Privacy</a>
-        </div>
-        <div className="mr-1">
-          <a className="cursor-pointer text-gray-700 ">Terms</a>
-        </div>
+        {footerButtonsNames.map((footerButtonsName) => (
+          <div className="mr-1" key={footerButtonsName}>
+            <a className="cursor-pointer text-gray-700 ">{footerButtonsName}</a>
+          </div>
+        ))}
       </div>
 
       <div className="flex">
-        <div className="mr-2">
+        <div className="mr-1">
           <a className="cursor-pointer text-gray-700 ">Text to speech</a>
         </div>
-        <div className="mr-2">
+        <div className="mr-1">
           <a className="cursor-pointer text-gray-700 ">Teams</a>
         </div>
       </div>
