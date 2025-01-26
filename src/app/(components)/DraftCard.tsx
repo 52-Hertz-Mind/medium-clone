2971d56d479e43cbbe21bf4e3f01335738cac0cd
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2025-01-26 10:31:49 +0330

    [#12-ADD] draftcard component

--- src/app/(components)/DraftCard.tsx
+++ src/app/(components)/DraftCard.tsx
@@ -1,3 +1,8 @@
+"use client";
+import { ChevronDown } from "lucide-react";
+import SmallDialog from "@/app/(components)/SmallDialog";
+import { useState } from "react";
+
 export interface DraftCardDataModel {
   title: string;
   description: string;
@@ -10,9 +15,25 @@ interface DraftCardData {
 }
 
 function DraftCard({ data }: DraftCardData) {
+  const [isSmallDialogActive, setIsSmallDialogActive] = useState(false);
   return (
-    <div>
-      <h2>{data.title}</h2>
+    <div className={"flex flex-col gap-1 border-b pb-6 pt-8"}>
+      <h2 className={"font-bold text-sm"}>{data.title}</h2>
+      <p className={"text-gray-500 text-sm"}>{data.description}</p>
+      <div className={"flex gap-1 font-[3px] text-gray-500 text-sm mt-2"}>
+        <span>last edited {data.lastEdited}</span>
+        <span>.</span>
+        <span>{data.readTime} min read</span>
+        <span>({data.wordCount} words) so far</span>
+        <div onClick={() => setIsSmallDialogActive(true)}>
+          <ChevronDown strokeWidth={1} className={"cursor-pointer"} />
+          <SmallDialog
+            links={["Edit draft", "Delete draft"]}
+            isActive={isSmallDialogActive}
+            onClose={() => setIsSmallDialogActive(false)}
+          />
+        </div>
+      </div>
     </div>
   );
 }
