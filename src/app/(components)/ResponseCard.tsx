d4b487e9a5a2180e7b7f957f02da64dc0ba8a4b6
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2025-01-29 11:11:42 +0330

    [#12-ADD] implement ResponseCard component

--- src/app/(components)/ResponseCard.tsx
+++ src/app/(components)/ResponseCard.tsx
@@ -1,3 +1,7 @@
+"use client";
+import { Ellipsis, Share } from "lucide-react";
+import SmallDialog from "@/app/(components)/SmallDialog";
+import { useState } from "react";
 //region data models
 interface ResponseCardDataModel {
   title: string;
@@ -9,7 +13,43 @@ interface ResponseCardData {
 }
 //endregion
 function ResponseCard({ data }: ResponseCardData) {
-  return <div></div>;
+  //region hooks
+  const [isShareDialgOpen, setIsShareDialgOpen] = useState(false);
+  const [isMoreDialogOpen, setIsMoreDialogOpen] = useState(false);
+  //endregion
+  return (
+    <div className="flex flex-col gap-2 pt-10 border-b pb-8">
+      <h2 className="font-bold cursor-pointer">{data.title}</h2>
+      <div className="flex gap-1 text-sm text-gray-500">
+        <p>Published on {data.publishDate}</p>
+        <span>.</span>
+        <p>{data.readTime} min read</p>
+        <div className="flex gap-2 ml-2">
+          <button onClick={() => setIsShareDialgOpen(true)}>
+            <Share size={20} strokeWidth={1.2} />
+            <SmallDialog
+              links={[
+                "Copy link",
+                "Share on X",
+                "Share on Facebook",
+                "Share on Linkedin",
+              ]}
+              isActive={isShareDialgOpen}
+              onClose={() => setIsShareDialgOpen(false)}
+            />
+          </button>
+          <button onClick={() => setIsMoreDialogOpen(true)}>
+            <Ellipsis />
+            <SmallDialog
+              links={["Edit response", "View stats", "Delete response"]}
+              isActive={isMoreDialogOpen}
+              onClose={() => setIsMoreDialogOpen(false)}
+            />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ResponseCard;
--- src/app/me/stories/responses/page.tsx
+++ src/app/me/stories/responses/page.tsx
@@ -1,5 +1,17 @@
+import ResponseCard from "@/app/(components)/ResponseCard";
+
 function Page() {
-  return <div>responses</div>;
+  const responseCardData = {
+    title: "Nice topic",
+    publishDate: "Aug 13, 2023",
+    readTime: 1,
+  };
+  return (
+    <div>
+      <ResponseCard data={responseCardData} />
+      <ResponseCard data={responseCardData} />
+    </div>
+  );
 }
 
 export default Page;
